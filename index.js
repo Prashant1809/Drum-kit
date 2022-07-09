196ace2da8bace804298d8d08294fcb65e208df6
prashant chaudhayr <prashantckota45@gmail.com>
2022-07-09 11:10:15 +0530

    drum-kit using  java script

---
+++ index.js
@@ -0,0 +1,130 @@
+/* document.querySelector("button").addEventListener("click", gotclicked);
+function gotclicked() {
+  alert("i got cliked");
+} */
+/* document.querySelector("button").addEventListener("click", function () {
+  alert("i got cliked");
+}); */
+/* var numberofbutton = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberofbutton; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    alert("i got clicked");
+  });
+} */
+/* var numberofbutton = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberofbutton; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+  });
+} */
+/* var numberofbutton = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberofbutton; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    console.log(this);
+  }); */
+/* var numberofbutton = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberofbutton; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    console.log(this.innerHTML);
+  });
+} */
+/* var numberofbutton = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberofbutton; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    this.style.color = "white";
+  });
+} */
+/* var numberofbutton = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberofbutton; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var buttoninnerhtml = this.innerHTML;
+    switch (buttoninnerhtml) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+    }
+  });
+} */
+var numberofbutton = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberofbutton; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var buttoninnerhtml = this.innerHTML;
+    makesound(buttoninnerhtml);
+    buttonanimation(buttoninnerhtml);
+  });
+}
+
+/* document.addEventListener("keypress", function () {
+  alert("key i s pressed");
+}); */
+/* document.addEventListener("keypress", function (event) {
+  console.log(event);
+}); */
+document.addEventListener("keypress", function (event) {
+  makesound(event.key);
+  buttonanimation(event.key);
+});
+function makesound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+  }
+}
+function buttonanimation(currkey) {
+  document.querySelector("." + currkey).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector("." + currkey).classList.remove("pressed");
+  }, 100);
+}
